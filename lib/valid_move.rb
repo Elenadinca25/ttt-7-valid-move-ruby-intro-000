ba7b9941f40458bafe9cadf70f8209073f4d5a8d
Elena Dinca <elenadinca25@gmail.com>
2020-02-24 23:20:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,12 @@
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
-  if position_taken? == valid_move?
-    return true
-  elsereturn false
 end
+
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
     return true
   end
+end
 end
