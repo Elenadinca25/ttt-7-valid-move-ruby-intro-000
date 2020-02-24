91d192d7c55952be529ed8ef58de6a4d21b3f56b
Elena Dinca <elenadinca25@gmail.com>
2020-02-24 23:11:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,8 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board,position)
+  position.to_i.between(1, 9) and not position_taken?(board, position.to_i - 1)
+  def position_taken?(board,index)
+    return false if [" ", "", nil].include?(board[index])
+    return true if ["X", "O"].include?(board[index])
+    puts "#{board[index]} is not a valid move"
+end
+end
